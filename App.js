f202264838bc38fc70ecb43b7a7c2484dfb21bbd
shyam66666 <peddishyamkumar@gmail.com>
2024-12-30 21:33:34 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1",{id:"heading"},"hello world!");
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ console.log(heading)
+root.render(heading);
